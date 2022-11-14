85162b1c705be098e98a88280fc99946c082ff08
buty <buty4649@gmail.com>
2022-11-14 22:54:28 +0900

    Remove -pv option
    `mkr plugin install` assumes no version is included

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-VERSION=$(awk '/VERSION/{gsub(/"/,"");print $NF}' lib/version.go)
 GOLANG_VERSION=1.19.3
 NAME=mackerel-plugin-thermal
 
 docker run -i --rm -v $(pwd):/usr/src/$NAME -w /usr/src/$NAME golang:$GOLANG_VERSION bash <<EOS
 go install github.com/Songmu/goxz/cmd/goxz@latest
 go mod tidy
-goxz -pv $VERSION -z -os linux -arch amd64,arm64
+goxz -z -os linux -arch amd64,arm64
 EOS
